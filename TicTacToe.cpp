94a3c2eeca8c34a7a2b0ecb5b3ade0100662bed0
Benbenbenin0 <70293945+Benbenbenin0@users.noreply.github.com>
2021-09-23 21:37:30 -0600

    Update TicTacToe.cpp

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -8,6 +8,17 @@ std::vector<std::vector<int>> CreateBoard()
    return std::vector<std::vector<int>>(3, std::vector<int>(3, 0));
 }
 
+void DisplayBoard(std::vector<std::vector<int>> vec){
+   for (int i = 0; i < vec.size(); i++)
+   {
+    for (int j = 0; j < vec[i].size(); j++)
+    {
+        cout << vec[i][j];
+    }
+      cout << endl;
+   }
+}
+
 main(){
   std::vector<std::vector<int>> board = CreateBoard();
 }
