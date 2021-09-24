caa99dc50024f32034b61a931cc1f7ad3a3180fd
Kevin Vo <57504309+kevinpvo@users.noreply.github.com>
2021-09-23 21:42:30 -0600

    Update TicTacToe.cpp

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -8,14 +8,6 @@ std::vector<std::vector<int>> CreateBoard()
    return std::vector<std::vector<int>>(3, std::vector<int>(3, 0));
 }
 
-void DisplayBoard(auto board){
-    for (int i = 0; i < 3; i++){
-        for (int j = 0; j < 3; j++){
-            std::cout << board[i][j] << " ";
-        }
-        std::cout << std::endl;
-    }
-}
 
 main(){
   std::vector<std::vector<int>> board = CreateBoard();
